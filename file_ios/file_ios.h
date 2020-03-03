28cdb145b50b56b621dc325fdac100d74e4fd0bc
Terry Chou <terrychoucn@gmail.com>
2020-03-03 15:14:32 +0800

    Initial Commit

---
+++ file_ios/file_ios.h
@@ -0,0 +1,19 @@
+//
+//  file_ios.h
+//  file_ios
+//
+//  Created by Terry Chou on 3/3/20.
+//  Copyright © 2020 Boogaloo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for file_ios.
+FOUNDATION_EXPORT double file_iosVersionNumber;
+
+//! Project version string for file_ios.
+FOUNDATION_EXPORT const unsigned char file_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <file_ios/PublicHeader.h>
+
+
